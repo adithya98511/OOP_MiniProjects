e4bf44922bd9ebe99fe15a1ed5b47fe30919c1b8
Adi511 <adithyarathnayake98511@gmail.com>
2023-11-10 00:10:02 +0530

    Defined askDepartment method

--- Email Application/src/Email.java
+++ Email Application/src/Email.java
@@ -1,10 +1,55 @@
+import java.util.Scanner;
+
 public class Email
 {
-    // Declare variables and assign values
+    // Declare variables
+
+    private String firstname;
+    private String lastname;
+    private String department;
+    private String password;
+    private int mailboxCapacity;
+    private String altEmail;
+
 
     //contructor for getting fname and lname
 
-    // ask for department
+    public Email(String firstname, String lastname)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        // class var   // var we pass from the constructor
+
+        //checking for the result
+        System.out.println("Email created for "+this.firstname + " " + this.lastname);
+
+       this.department = askDepartment();
+        System.out.println("Department is :"+department);
+
+    }
+
+
+    private String askDepartment()
+    {
+        System.out.println("Enter your Department:");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        if (choice == 1)
+        {
+            return "Sales";
+        } else if (choice == 2)
+        {
+         return "Development";
+        } else if (choice == 3)
+        {
+         return "Other";
+        }else
+        {
+            return  "None";
+        }
+
+    }
 
     //Generate a random pw
 
--- Email Application/src/EmailApp.java
+++ Email Application/src/EmailApp.java
@@ -1,3 +1,8 @@
 public class EmailApp
 {
+    public static void main(String[] args) {
+
+        Email email1 = new Email("Adithya","Rathnayake");
+
+    }
 }
