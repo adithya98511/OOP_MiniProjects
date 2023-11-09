af46b280f1f681107ee34b88f9cbe0c3589074e9
Adi511 <adithyarathnayake98511@gmail.com>
2023-11-09 23:32:25 +0530

    finalized the methods

--- Email Application/src/Email.java
+++ Email Application/src/Email.java
@@ -1,2 +1,19 @@
-public class Email {
+public class Email
+{
+    // Declare variables and assign values
+
+    //contructor for getting fname and lname
+
+    // ask for department
+
+    //Generate a random pw
+
+    // set the mailbox capacity
+
+    //set the alternative email
+
+    //change your password
+
+    //get mailbox,alternative email
+
 }
--- Email Application/src/EmailApp.java
+++ Email Application/src/EmailApp.java
@@ -1,2 +1,3 @@
-public class EmailApp {
+public class EmailApp
+{
 }
